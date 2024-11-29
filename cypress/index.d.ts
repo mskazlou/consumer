7ad0e02b524eb5f4e444dd88f9484f9dde94027f
Mikhail Kazlou1 <kazlou.ms@gmail.com>
2024-11-29 15:03:13 +0200

    add cypress commands

--- cypress/index.d.ts
+++ cypress/index.d.ts
@@ -1,8 +1,75 @@
+import type { Movie } from '../src/consumer'
+
 export {}
 
 declare global {
   namespace Cypress {
     interface Chainable<Subject> {
+      /** Gets a list of movies
+       * ```js
+       * cy.getAllMovies(token)
+       * ```
+       */
+      getMovies(
+        token: string,
+        allowedToFail?: boolean
+      ): Chainable<Response<Movie[]> & Messages>
+
+      /** Gets a movie by id
+       * ```js
+       * cy.getMovieById(token, 1)
+       * ```
+       */
+      getMovieById(
+        token: string,
+        id: number,
+        allowedToFail?: boolean
+      ): Chainable<Response<Movie> & Messages>
+
+      /** Gets a movie by name
+       * ```js
+       * cy.getMovieByName(token, 'The Great Gatsby')
+       * ```
+       */
+      getMovieByName(
+        token: string,
+        name: string,
+        allowedToFail?: boolean
+      ): Chainable<Response<Movie> & Messages>
+
+      /** Creates a movie
+       * ```js
+       * cy.addMovie({name: 'The Great Gatsby'})
+       * ```
+       */
+      addMovie(
+        token: string,
+        body: Omit<Movie, 'id'>,
+        allowedToFail?: boolean
+      ): Chainable<Response<Movie> & Messages>
+
+      /** Updates a movie by id
+       * ```js
+       * cy.updateMovie(1, {name: 'The Great Gatsby'})
+       * ```
+       */
+      updateMovie(
+        token: string,
+        id: number,
+        body: Partial<Omit<Movie, 'id'>>
+      ): Chainable<Response<Movie> & Messages>
+
+      /** Deletes a  movie
+       * ```js
+       * cy.deleteMovie(1)
+       * ```
+       */
+      deleteMovie(
+        token: string,
+        id: number,
+        allowedToFail?: boolean
+      ): Chainable<Response<Movie> & Messages>
+
       /** https://www.npmjs.com/package/@cypress/skip-test
        * `cy.skipOn('localhost')` */
       skipOn(
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,3 +1,39 @@
 import './commands'
 import 'cypress-map'
 import '@bahmutov/cy-api'
+import type { Movie } from '../../src/consumer'
+
+const apiUrl = Cypress.config('baseUrl') || 'http://localhost:3001'
+
+Cypress.Commands.add('getMovies', (url = apiUrl) => {
+  cy.log('**getAllMovies**')
+  return cy.task('getAllMovies', url)
+})
+
+Cypress.Commands.add('getMovieById', (url: string, id: number) => {
+  cy.log(`**getMovieById ${id}**`)
+  return cy.task('getMovieById', { url, id })
+})
+
+Cypress.Commands.add('getMovieByName', (url: string, name: string) => {
+  cy.log(`**getMovieByName ${name}**`)
+  return cy.task('getMovieByName', { url, name })
+})
+
+Cypress.Commands.add('addMovie', (url: string, data: Omit<Movie, 'id'>) => {
+  cy.log(`**addMovie: ${JSON.stringify(data)}**`)
+  return cy.task('addMovie', { url, data })
+})
+
+Cypress.Commands.add('deleteMovie', (url: string, id: number) => {
+  cy.log(`**deleteMovie ${id}**`)
+  return cy.task('deleteMovie', { url, id })
+})
+
+Cypress.Commands.add(
+  'updateMovie',
+  (url: string, id: number, data: Partial<Movie>) => {
+    cy.log(`**updateMovie ${id} ${JSON.stringify(data)}**`)
+    return cy.task('updateMovie', { url, id, data })
+  }
+)
